ff399958514a8132ad3d71438622bb8ea5d24a1d
Santiago Caraballo <48489427+sancara@users.noreply.github.com>
2021-08-19 00:22:57 -0300

    Name DOM modification

--- final-javascript.js
+++ final-javascript.js
@@ -3,3 +3,11 @@ const getDataFromGit = async () => {
   const responseJson = await response.json();
   return responseJson[0];
 };
+
+const getProfileData = async () => {
+  const profileData = await getDataFromGit();
+  const name = await profileData.owner.login;
+  
+  const nameNode = document.querySelector('h1');
+  nameNode.textContent = name;
+}
