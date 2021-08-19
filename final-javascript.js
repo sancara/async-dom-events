ca93214f6cbe33cd462b067b6eefa42af14df6c5
Santiago Caraballo <48489427+sancara@users.noreply.github.com>
2021-08-19 00:18:47 -0300

    async and fetch from gitHub API

---
+++ final-javascript.js
@@ -0,0 +1,5 @@
+const getDataFromGit = async () => {
+  const response = await fetch("https://api.github.com/repositories");
+  const responseJson = await response.json();
+  return responseJson[0];
+};
